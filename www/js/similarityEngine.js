398d20c94fc0403ee82e971f1290e846c68daa31
siwrad82 <siwrad86@gmail.com>
2026-07-26 13:46:41 +0700

    Build 016.0 - Similarity Engine v2

--- www/js/similarityEngine.js
+++ www/js/similarityEngine.js
@@ -2,8 +2,9 @@
 ====================================
 SIWRAD Vision
 Similarity Engine
-Version : 1.0.0
-Build   : 015
+Version : 2.0.0
+Build   : 016
+Codename: Similarity Engine v2
 ====================================
 */
 
@@ -13,32 +14,56 @@ const SimilarityEngine = {
 
         let score = 0;
 
-        // Prototype:
-        // Hitung jumlah foto yang dimiliki dataset
+        const detail = {
+            dataset: 0,
+            category: 0,
+            name: 0,
+            visual: 0
+        };
+
+        // Dataset Score (0 - 30)
 
         if(product.photos){
 
-            Object.values(product.photos).forEach(function(photo){
+            const totalPhotos = Object.values(product.photos)
+                .filter(photo => photo).length;
 
-                if(photo){
-                    score++;
-                }
+            detail.dataset = Math.round(
+                (totalPhotos / 8) * 30
+            );
 
-            });
+            score += detail.dataset;
 
         }
 
+        // Category Score (0 - 30)
+
+        if(scanData.category &&
+           scanData.category === product.category){
+
+            detail.category = 30;
+            score += detail.category;
+
+        }
+
+        // Product Name Score (0 - 20)
+
+        if(scanData.name &&
+           scanData.name === product.name){
+
+            detail.name = 20;
+            score += detail.name;
+
+        }
+
+        // Visual Score
+        // Reserved Build 016.1
+
         return {
 
-            id: product.id,
-
-            name: product.name,
-
-            category: product.category,
-
-            score: Math.round((score / 8) * 100),
-
-            product: product
+            product: product,
+            score: score,
+            detail: detail
 
         };
 
@@ -46,23 +71,23 @@ const SimilarityEngine = {
 
     rank(scanData, products){
 
-        const results = [];
+        const ranking = [];
 
-        products.forEach(function(product){
+        products.forEach((product)=>{
 
-            results.push(
+            ranking.push(
                 this.calculate(scanData, product)
             );
 
-        }, this);
+        });
 
-        results.sort(function(a, b){
+        ranking.sort((a,b)=>{
 
             return b.score - a.score;
 
         });
 
-                return results;
+        return ranking;
 
     }
 
