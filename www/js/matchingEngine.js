7ec07ca40d7a0c77464352ea2356c94077c9d910
siwrad82 <siwrad86@gmail.com>
2026-07-26 13:24:11 +0700

    Build 015 Stable - Fix Matching Engine and Recognition

--- www/js/matchingEngine.js
+++ www/js/matchingEngine.js
@@ -8,20 +8,23 @@ Build   : 014
 */
 
 const MatchingEngine = {
-match(scanData){
 
-    const products = DatabaseEngine.getProducts();
+    match(scanData){
 
-    if(products.length === 0){
-        return null;
+        const products = DatabaseEngine.getProducts();
+
+        if(products.length === 0){
+            return null;
+        }
+
+        const ranking = SimilarityEngine.rank(scanData, products);
+
+        return {
+            product: ranking[0].product,
+            similarity: ranking[0].score,
+            ranking: ranking
+        };
+
     }
 
-    const ranking = SimilarityEngine.rank(scanData, products);
-
-    return {
-        product: ranking[0].product,
-        similarity: ranking[0].score,
-        ranking: ranking
-    };
-
-}
+};
